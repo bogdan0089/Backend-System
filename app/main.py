ffb0235b8eef5bb1ce2a0c6a16d693a1761977c7
bohdan0089 <bodiks18@gmail.com>
2026-03-16 13:52:14 +0100

    init

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from database.database import async_engine, Base
-from router_client import router_client
-from router_orders import router_order
-from router_products import router_product
-from router_transaction import router_transaction
+from app.router_client import router_client
+from app.router_orders import router_order
+from app.router_products import router_product
+from app.router_transaction import router_transaction
 
 app = FastAPI()
 
